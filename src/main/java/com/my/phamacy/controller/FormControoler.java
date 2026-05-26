5854aa840b15707b8c946467ee9cf08a46fb8ef7
chldnjfghks-spec <chldnjfghks@gmail.com>
2026-05-26 14:51:51 +0900

    feat : 주소로 검색하기 서비스구성 완료

--- src/main/java/com/my/phamacy/controller/FormControoler.java
+++ src/main/java/com/my/phamacy/controller/FormControoler.java
@@ -1,11 +1,21 @@
 package com.my.phamacy.controller;
 
 
+import com.my.phamacy.dto.DocumentDto;
+import com.my.phamacy.dto.KakaoApiResponseDto;
+import com.my.phamacy.service.KakaoAddressSearchService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
+@RequiredArgsConstructor
+@Slf4j
 public class FormControoler {
+    private final KakaoAddressSearchService kakaoAddressSearchService;
     @GetMapping
     public String mainForm(){
         return "main";
@@ -13,6 +23,17 @@ public class FormControoler {
 
     @GetMapping("/output")
     public String outputForm(){
+        return "output";
+    }
+
+    @PostMapping("/search")
+    public String searchAddress(@RequestParam("address")String address){
+        KakaoApiResponseDto kakaoApiResponseDto = kakaoAddressSearchService.requestAddressSearch(address);
+        log.info("결과 :" + kakaoApiResponseDto);
+        // 결과중 Documents만 빼서 dto에 저장
+        DocumentDto documentDto = kakaoApiResponseDto
+                .getDocumentList().get(0);
+        log.info("도큐먼트 만 출력 :" + documentDto);
         return "output";
     }
 }
---
+++ src/main/java/com/my/phamacy/dto/KakaoApiResponseDto.java
@@ -0,0 +1,18 @@
+package com.my.phamacy.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.util.List;
+
+@Getter
+@Setter
+@ToString
+public class KakaoApiResponseDto {
+    private MetaDto metaDto;
+
+    @JsonProperty("documents")
+    private List<DocumentDto> documentList;
+}
